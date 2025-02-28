ce45708ca9bcc91c7159daa8cdddb3fdff836631
Karolis Beliasas <karolis.beliasas@nfq.com>
2025-02-28 14:45:06 +0200

    Crafting update

--- src/main/java/com/kbeliasas/everything/skills/crafting/CraftingConfig.java
+++ src/main/java/com/kbeliasas/everything/skills/crafting/CraftingConfig.java
@@ -1,5 +1,10 @@
 package com.kbeliasas.everything.skills.crafting;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 public class CraftingConfig {
 
     private static final int SILVER_BAR = 2355;
@@ -18,7 +23,26 @@ public class CraftingConfig {
         JEWELLERY
     }
 
+    @RequiredArgsConstructor
+    @Getter
     public enum JewelleryProducts {
-        TIARA
+        TIARA(5523, 5525, List.of(Resource.SILVER_BAR), new int[]{6, 28, 3}),
+        DIAMOND_NECKLACE(1597, 1662, List.of(Resource.GOLD_BAR, Resource.DIAMOND),  new int[]{446, 27});
+
+        private final Integer mouldId;
+        private final Integer productId;
+        private final List<Resource> resources;
+        private final int[] widgetIds;
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    public enum Resource {
+        SILVER_BAR(2355, 27),
+        GOLD_BAR(2357, 13),
+        DIAMOND(1601, 13);
+
+        private final Integer id;
+        private final Integer amount;
     }
 }
