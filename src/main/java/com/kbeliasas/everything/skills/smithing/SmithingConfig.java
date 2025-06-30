faa69bde82197600a337729f4eb6da9fc5872d9c
Karolis Beliasas <karolis.beliasas@nfq.com>
2025-06-30 15:22:53 +0300

    Add mithril ore and bar support to SmithingConfig

--- src/main/java/com/kbeliasas/everything/skills/smithing/SmithingConfig.java
+++ src/main/java/com/kbeliasas/everything/skills/smithing/SmithingConfig.java
@@ -11,6 +11,7 @@ public class SmithingConfig {
     private static final int SILVER_ORE = 442;
     private static final int IRON_ORE = 440;
     private static final int COAL_ORE = 453;
+    private static final int MITHRIL_ORE = 447;
     private static final Area FURNACE = new Area(3105, 3501, 3109, 3496);
 
     public enum Type {
@@ -21,7 +22,8 @@ public class SmithingConfig {
     @Getter
     public enum Bar {
         SILVER(List.of(new OreInfo(SILVER_ORE, 28, 1)),16),
-        STEEL(List.of(new OreInfo(IRON_ORE, 9, 1), new OreInfo(COAL_ORE, 18, 2)), 17);
+        STEEL(List.of(new OreInfo(IRON_ORE, 9, 1), new OreInfo(COAL_ORE, 18, 2)), 17),
+        MITHRIL(List.of(new OreInfo(MITHRIL_ORE, 5, 1), new OreInfo(COAL_ORE, 20, 4)), 19);
 
         private final List<OreInfo> oreInfos;
         private final Integer widgetId;
@@ -30,7 +32,8 @@ public class SmithingConfig {
     @RequiredArgsConstructor
     @Getter
     public enum Platebody {
-        STEEL(2353);
+        STEEL(2353),
+        MITHRIL(2359);
 
         private final Integer barId;
     }
